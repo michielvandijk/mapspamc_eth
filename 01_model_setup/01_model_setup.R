549d38c6da368332c13c165acc2942adb5160176
Dijk <michiel.vandijk@wur.nl>
2023-01-02 14:19:40 +0100

    change gams version

--- 01_model_setup/01_model_setup.R
+++ 01_model_setup/01_model_setup.R
@@ -37,7 +37,7 @@ model_path <- "C:/Users/dijk158/OneDrive - Wageningen University & Research/data
 db_path <- "c:/temp"
 
 # Sets the location of the version of GAMS that will be used to solve the model
-gams_path <- "C:/MyPrograms/GAMS/35"
+gams_path <- "C:/MyPrograms/GAMS/40"
 
 # Set mapspamc parameters for the min_entropy_5min_adm_level_2_solve_level_0 model
 param <- mapspamc_par(
